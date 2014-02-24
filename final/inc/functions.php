3344b833585794ed16b0095271293ea552ef7ae6
unknown <ParikWin7@.(none)>
2014-02-24 08:51:05 -0500

    hi

--- final/inc/functions.php
+++ final/inc/functions.php
@@ -1,5 +1,20 @@
 <?php
+	include_once 'passwprd.php';
+	
     function GetConnection()
 	{
+		global $password;
+		
+		//aim is to return an objest for mysql
+		$conn = mysqli_connect('localhost', 'panwarn1', $password, 'panwarn1_db');
+		return $conn;
+		
+		//echo(mysqli_error($conn));
+		//echo 'called';
+		
+		// we do not put password coz we are cheking in github so we dont want it to be open
 		
 	}
+	
+	//$conn = GetConnection();
+	//print_r($conn);  these two is for test connection
